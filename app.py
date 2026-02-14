d710565d0a589efbeb49d26bb281dd4c5e94c3dd
Stacy <stacyywere@gmail.com>
2026-02-15 01:42:45 +0300

    Streamlit test

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🌍 Safari Tour Recommender")
+
+# This is a placeholder to test if it works
+st.write("Welcome! This app will recommend tours based on user specifications.")
